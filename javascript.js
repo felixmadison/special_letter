4f2b4c01c119b6b32b35895ed5e1848dbc7ced9a
felixmadison <158294534+felixmadison@users.noreply.github.com>
2024-11-13 14:05:09 +0700

    Update javascript.js
    Signed-off-by: felixmadison <158294534+felixmadison@users.noreply.github.com>

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const text =
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+"Hai Sherline, sebenarnya sudah lama aku ingin menyampaikan ini, tapi terus terang aku butuh keberanian lebih. Semakin lama kita mengenal, ada perasaan yang tumbuh dalam hatiku dan aku tidak bisa lagi menyimpannya sendiri. Sejak pertama kali kita bertemu, sejak kita satu kelompok, aku mulai merasa bahwa ada sesuatu yang istimewa dalam dirimu. Sesuatu yang membuat aku selalu menanti senyumanmu. Kamu berbeda dari yang lain, bukan hanya karena kebaikan atau tawamu, tapi karena caramu membuat segalanya di sekitarmu terasa lebih indah. Aku suka kamu bukan sekadar sebagai teman, tapi lebih dari itu. Aku tidak tahu bagaimana perasaanmu, dan apapun itu aku akan menghargainya. Aku hanya ingin kita tetap berteman apapun jawabanmu. Terima kasih sudah membaca confessku ini. Mengenalmu saja sudah sangat berarti buatku. Aku berharap kita bisa lebih dekat, apapun yang terjadi.";
 const paragraph = text.split("");
 
 let i = 0;
@@ -19,9 +19,9 @@ function dashOut(arr) {
 function interval(letter) {
   console.log(letter);
   if (letter == ";" || letter == "." || letter == ",") {
-    return Math.floor(Math.random() * 500 + 500); 
+    return Math.floor(Math.random() * 200 + 300); 
   } else {
-    return Math.floor(Math.random() * 130 + 5); 
+    return Math.floor(Math.random() * 100 + 5); 
   }
 }
 
